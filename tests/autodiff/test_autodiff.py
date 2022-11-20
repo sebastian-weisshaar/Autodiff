5bf2b4f6a25f78387740501b5d67ff94546ca9e7
Sebastian <sweisshaar@g.harvard.edu>
2022-11-20 14:32:58 -0500

    New test functions for functions.py

--- tests/autodiff/test_autodiff.py
+++ tests/autodiff/test_autodiff.py
@@ -1,1 +1,9 @@
 
+import sys
+sys.path.insert(1, './src/autodiff')
+import numpy as np
+
+from autodiff import AutoDiff
+from autodiff import Node
+
+
--- tests/autodiff/test_functions.py
+++ tests/autodiff/test_functions.py
@@ -1,7 +1,19 @@
 import sys
 sys.path.insert(1, './src/autodiff')
-from functions import sin
+from functions import sin,cos,tan
 import numpy as np
 
-def test_sin():
-    assert(sin(12)==np.sin(12))
+
+class TestFunction:
+    def test_sin():
+        test_number=np.random.normal(0,100)
+        assert(sin(test_number)==np.sin(test_number))
+    def test_cos():
+        test_number=np.random.normal(0,100)
+        assert(cos(test_number)==np.cos(test_number))
+    def test_tan():
+        test_number=np.random.normal(0,100)
+        assert(tan(test_number)==np.tan(test_number))
+    
+    
+
