113d32279cbce8246db5e875ac5d72b95c00c2e1
Anna Midgley <amidgley@g.harvard.edu>
2022-12-09 21:45:51 -0500

    added option to run coverage or test in test harness

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,4 +2,10 @@
 # File       : run_tests.sh
 # Description: Test suite driver script
 
-pytest --cov=../src/autodiff --cov-fail-under=90 --cov-report html:cov_html
+if [ ${1} == 'coverage' ]
+then
+  pytest --cov=../src/autodiff --cov-fail-under=90 --cov-report html:cov_html
+elif [ ${1} == 'test' ]
+then
+  pytest
+fi
